340e2c63d8b4d841275cfb3bb7942f3bced8f86e
Sönke Michelsen <s.michelsen@axovis.com>
2016-07-13 15:50:22 +0200

    [TASK] added precision option for rounding to math viewhelper

--- Classes/Axovis/Flow/Utilities/ViewHelpers/MathViewHelper.php
+++ Classes/Axovis/Flow/Utilities/ViewHelpers/MathViewHelper.php
@@ -9,18 +9,19 @@ class MathViewHelper extends AbstractViewHelper {
     /**
      * @param string $value
      * @param boolean $round
+     * @param int $precision the precision if rounding
      * @return string
      * @throws Exception
      */
-    public function render($value,$round = false) {
-        if(preg_match('/^[\d\., \+\-\*\/\(\)]/',$value) === 1) {
+    public function render($value,$round = false,$precision = 0) {
+        if(preg_match('/^[\d\., \+\-\*\/\(\)]*$/',$value) === 1) {
             $result = eval('return ' . $value . ';');
             if($round) {
-		$result = round($result);
-	    }
-	    return $result;
+                $result = round($result,$precision);
+            }
+            return $result;
         } else {
-            throw new Exception('invalid value');
+            throw new Exception("invalid value \"$value\"");
         }
     }
 }
