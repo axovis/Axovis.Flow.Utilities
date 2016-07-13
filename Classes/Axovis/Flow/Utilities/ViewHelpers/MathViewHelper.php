adb36cc49144802fc76ad9edbebccd677ade6fd9
Sönke Michelsen <s.michelsen@axovis.com>
2016-07-13 15:30:28 +0200

    [TASK] added round option to math view helper

--- Classes/Axovis/Flow/Utilities/ViewHelpers/MathViewHelper.php
+++ Classes/Axovis/Flow/Utilities/ViewHelpers/MathViewHelper.php
@@ -8,12 +8,17 @@ use TYPO3\Flow\Annotations as Flow;
 class MathViewHelper extends AbstractViewHelper {
     /**
      * @param string $value
+     * @param boolean $round
      * @return string
      * @throws Exception
      */
-    public function render($value) {
-        if(preg_match('/^[\d\., \+\-\*\/]/',$value) === 1) {
-            return eval('return ' . $value . ';');
+    public function render($value,$round = false) {
+        if(preg_match('/^[\d\., \+\-\*\/\(\)]/',$value) === 1) {
+            $result = eval('return ' . $value . ';');
+            if($round) {
+		$result = round($result);
+	    }
+	    return $result;
         } else {
             throw new Exception('invalid value');
         }
